0f7112cda92724d20c622c14f8c6622663fff150
lihao <lihao@yazuishou.com>
2021-11-01 09:46:32 +0800

    dsp-daily-test: 新增日常测试模块

---
+++ daily-test/daily-test-simple/src/main/java/com/lz/DailyTestSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.lz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyTestSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyTestSimpleApplication.class, args);
+    }
+
+}
---
+++ daily-test/daily-test-simple/src/test/java/com/lz/DailyTestSimpleApplicationTests.java
@@ -0,0 +1,14 @@
+package com.lz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyTestSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello world");
+    }
+
+}
