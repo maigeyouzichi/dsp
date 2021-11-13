ef8e4af78a13cf200dba66f3a97c876d52ac4e59
lihao <lihao@yazuishou.com>
2021-11-13 17:36:45 +0800

    Eureka集群搭建,单机模拟

---
+++ lagou-resume/lagou-cloud-eureka-server-8761/src/main/java/com/lz/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.lz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author lihao
+ * {@code @EnableEurekaServer} 声明当前项目为Eureka服务
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
--- lagou-resume/lagou-cloud-eureka-server-8761/src/test/java/com/lz/EurekaServerApplicationTests.java
+++ lagou-resume/lagou-cloud-eureka-server-8761/src/test/java/com/lz/EurekaServerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LagouCloudEurekaServer8761ApplicationTests {
+class EurekaServerApplicationTests {
 
     @Test
     void contextLoads() {
