c5b2d86b8e8ca29b559a76e599434ead64236e00
lihao <243149100@qq.com>
2021-11-27 13:58:20 +0800

    leetcode 斐波那契数

--- leet-code/src/test/java/com/lz/dynamicProgramming/DynamicProgrammingSimple.java
+++ leet-code/src/test/java/com/lz/dynamicProgramming/DynamicProgrammingSimple.java
@@ -218,4 +218,26 @@ public class DynamicProgrammingSimple {
         return Math.min(dp[dp.length-2],dp[dp.length -1]);
     }
 
+    /**
+     * 509. 斐波那契数
+     * 斐波那契数，通常用 F(n) 表示，形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+     * F(0) = 0，F(1) = 1
+     * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+     * 给你 n ，请计算 F(n)
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/fibonacci-number
+     */
+    int fib(int n) {
+        if (n==0) return 0;
+        if (n==1) return 1;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+
 }
