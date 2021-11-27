dc6ba958180351dfb30fc24e546eec2f5dc65c26
lihao <243149100@qq.com>
2021-11-27 15:17:25 +0800

    leetcode 获取生成数组中的最大值

--- leet-code/src/test/java/com/lz/dynamicProgramming/DynamicProgrammingSimple.java
+++ leet-code/src/test/java/com/lz/dynamicProgramming/DynamicProgrammingSimple.java
@@ -278,4 +278,34 @@ public class DynamicProgrammingSimple {
         return dp[n];
     }
 
+    /**
+     * 1646. 获取生成数组中的最大值
+     * 给你一个整数 n 。按下述规则生成一个长度为 n + 1 的数组 nums ：
+     * nums[0] = 0
+     * nums[1] = 1
+     * 当 2 <= 2 * i <= n 时，nums[2 * i] = nums[i]
+     * 当 2 <= 2 * i + 1 <= n 时，nums[2 * i + 1] = nums[i] + nums[i + 1]
+     * 返回生成数组 nums 中的 最大 值。
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/get-maximum-in-generated-array
+     */
+    int getMaximumGenerated(int n) {
+        if (n==0) return 0;
+        if (n==1) return 1;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        int max = 1;
+        for (int i = 0; i < n + 1; i++) {
+            if (2*i >=2 && 2*i<=n) {
+                dp[2*i] = dp[i];
+            }
+            if (2*i+1>=2 && 2*i+1<=n) {
+                dp[2*i+1]= dp[i] + dp[i+1];
+            }
+            max = Math.max(max,dp[i]);
+        }
+        return max;
+    }
+
 }
