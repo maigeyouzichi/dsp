dc40c476f6cb41c65abd8e85815d84121a3d39a2
lihao <243149100@qq.com>
2021-11-27 13:53:19 +0800

    leetcode 最小体力爬楼梯

--- leet-code/src/test/java/com/lz/dynamicProgramming/DynamicProgrammingSimple.java
+++ leet-code/src/test/java/com/lz/dynamicProgramming/DynamicProgrammingSimple.java
@@ -197,4 +197,25 @@ public class DynamicProgrammingSimple {
         return flag;
     }
 
+    /**
+     * 746. 使用最小花费爬楼梯
+     * 数组的每个下标作为一个阶梯，第 i 个阶梯对应着一个非负数的体力花费值 cost[i]（下标从 0 开始）。
+     * 每当你爬上一个阶梯你都要花费对应的体力值，一旦支付了相应的体力值，你就可以选择向上爬一个阶梯或者爬两个阶梯。
+     * 请你找出达到楼层顶部的最低花费。在开始时，你可以选择从下标为 0 或 1 的元素作为初始阶梯。
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/min-cost-climbing-stairs
+     * 思路: dp[i]表示走到索引为i的阶梯需要的最小花费体力
+     */
+    int minCostClimbingStairs(int[] cost) {
+        if (cost.length == 1) return cost[0];
+        if (cost.length == 2) return Math.min(cost[0],cost[1]);
+        int[] dp = new int[cost.length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < cost.length; i++) {
+            dp[i] = Math.min(dp[i-1],dp[i-2])+cost[i];
+        }
+        return Math.min(dp[dp.length-2],dp[dp.length -1]);
+    }
+
 }
