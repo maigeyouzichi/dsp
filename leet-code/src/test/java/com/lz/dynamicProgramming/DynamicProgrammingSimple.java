79b725d43805d900fa6df3a8884da5345ebb19dc
lihao <243149100@qq.com>
2021-11-27 21:11:06 +0800

    leetcode 按摩师

--- leet-code/src/test/java/com/lz/dynamicProgramming/DynamicProgrammingSimple.java
+++ leet-code/src/test/java/com/lz/dynamicProgramming/DynamicProgrammingSimple.java
@@ -396,4 +396,27 @@ public class DynamicProgrammingSimple {
         }
         return max;
     }
+
+    /**
+     * 面试题 17.16. 按摩师
+     * 一个有名的按摩师会收到源源不断的预约请求，每个预约都可以选择接或不接。
+     * 在每次预约服务之间要有休息时间，因此她不能接受相邻的预约。
+     * 给定一个预约请求序列，替按摩师找到最优的预约集合（总预约时间最长），返回总的分钟数。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/the-masseuse-lcci
+     * 思路: dp[i]表示第i天接收预约最大总时长,还是基本的动态规划问题
+     */
+    int massage(int[] nums) {
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        if (nums.length == 2) return Math.max(nums[0],nums[1]);
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i-2]+nums[i],dp[i-1]);
+        }
+        return dp[nums.length-1];
+    }
 }
