46798b279d7c5393bfff0ebb3a0f18b5b0909703
lihao <243149100@qq.com>
2021-11-29 23:28:56 +0800

    springcloud 引入gateway

---
+++ lagou-resume/lagou-cloud-gateway-9002/src/main/java/com/lz/LagouCloudGateway9002Application.java
@@ -0,0 +1,13 @@
+package com.lz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagouCloudGateway9002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LagouCloudGateway9002Application.class, args);
+    }
+
+}
