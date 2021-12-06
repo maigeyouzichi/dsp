de20bf294aa6d40e9efe67bb448f35e0f5c3aab4
lihao <243149100@qq.com>
2021-12-06 18:41:52 +0800

    springcloud 引入nacos

--- lagou-resume/lagou-cloud-oauth-server-9999/src/test/java/com/lz/LagouCloudOauthServer9999ApplicationTests.java
+++ lagou-resume/lagou-cloud-oauth-server-9999/src/test/java/com/lz/LagouCloudOauthServer9999ApplicationTests.java
@@ -1,13 +1,18 @@
 package com.lz;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@SpringBootTest(classes = LagouCloudOauthServer9999Application.class)
+@RunWith(SpringRunner.class)
 class LagouCloudOauthServer9999ApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(new BCryptPasswordEncoder().encode("nacos"));
     }
 
 }
