6810e92ca8ae4c87749ee7bfb729e0109b4a7088
lihao <243149100@qq.com>
2022-01-10 11:59:41 +0800

    算法 -- leetcode 判断子序列是否成立

--- leet-code/src/test/java/com/lz/dynamicProgramming/DynamicProgrammingSimple.java
+++ leet-code/src/test/java/com/lz/dynamicProgramming/DynamicProgrammingSimple.java
@@ -123,7 +123,7 @@ public class DynamicProgrammingSimple {
 
     /**
      * 121. 买卖股票的最佳时机
-     * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+     * 给定一个数组 prices ，它的第i个元素prices[i] 表示一支给定股票第 i 天的价格。
      * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
      * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0
      * 来源：力扣（LeetCode）
@@ -196,6 +196,26 @@ public class DynamicProgrammingSimple {
             }
         }
         return flag;
+    }
+
+    boolean isSubsequence02(String s, String t) {
+        if (s.length() > t.length()) return false;
+        int index = -1;
+        char[] subChars = s.toCharArray();
+        char[] chars = t.toCharArray();
+        //遍历子串
+        out:
+        for (int i = 0; i < subChars.length; i++) {
+            char target = subChars[i];
+            for (int j = index+1; j < chars.length; j++) {
+                if (chars[j] == target) {
+                    index = j;
+                    continue out;
+                }
+            }
+            return false;
+        }
+        return true;
     }
 
     /**
