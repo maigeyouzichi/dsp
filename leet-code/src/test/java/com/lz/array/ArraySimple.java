c2e139dd7e2ef44e972e0b3d0cfde738e63bb68a
lihao <243149100@qq.com>
2022-01-13 19:21:31 +0800

    算法-数组-去除重复值

--- leet-code/src/test/java/com/lz/array/ArraySimple.java
+++ leet-code/src/test/java/com/lz/array/ArraySimple.java
@@ -1,5 +1,6 @@
 package com.lz.array;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,6 +23,33 @@ public class ArraySimple {
         }
         return null;
     }
+
+    /**
+     * 26. 删除有序数组中的重复项
+     *
+     * 1 2 3 4 3 3 3 4
+     * p=0 1 2
+     * q=1 2 3 4 5 6 7
+     * 思路: 双指针遍历数组:
+     *  如果p,q位置数据相同,q++
+     *  如果p,q位置数据不同,q下标数据和p+1下标数据交换,同时
+     */
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int p = 0;
+        int q = 1;
+        while (q < nums.length) {
+            if (nums[p]==nums[q]) {
+                q++;
+            }else {
+                nums[p+1] = nums[q];
+                p++;
+            }
+        }
+        return p+1;
+    }
+
+
 
 
 }
