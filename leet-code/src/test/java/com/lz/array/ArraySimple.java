bff3116f5092e751aabb9fa7df32923d5382cf81
lihao <243149100@qq.com>
2022-01-17 15:38:10 +0800

    算法-数组-消失的所有的数字

--- leet-code/src/test/java/com/lz/array/ArraySimple.java
+++ leet-code/src/test/java/com/lz/array/ArraySimple.java
@@ -409,6 +409,24 @@ public class ArraySimple {
     }
 
 
+    /**
+     * 448. 找到所有数组中消失的数字
+     */
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int[] arr = new int[nums.length+1];
+        for (int num : nums) {
+            arr[num] = 1;
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+
+
     @Test
     void test() {
         Map<Integer, Integer> map = new HashMap<>();
