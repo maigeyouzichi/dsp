fb8de625bc1e6696595c7ca9288d078e77864c73
lihao <243149100@qq.com>
2022-01-17 16:08:44 +0800

    算法-数组-分发饼干

--- leet-code/src/test/java/com/lz/array/ArraySimple.java
+++ leet-code/src/test/java/com/lz/array/ArraySimple.java
@@ -442,6 +442,26 @@ public class ArraySimple {
         return res;
     }
 
+    /**
+     * 455,分发饼干
+     */
+    public int findContentChildren(int[] g, int[] s) {
+        if (s.length == 0) return 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        while (j < s.length && i < g.length) {
+            if (s[j] >= g[i]) {
+                count ++;
+                i ++;
+            }
+            j ++;
+        }
+        return count;
+    }
+
 
     @Test
     void test() {
