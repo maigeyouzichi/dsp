940a9b70b6b7cc88123b1491072b6e728790d509
lihao <243149100@qq.com>
2022-01-18 18:35:36 +0800

    leetcode 数组中等难度 三数之和

--- leet-code/src/test/java/com/lz/array/ArrayMid.java
+++ leet-code/src/test/java/com/lz/array/ArrayMid.java
@@ -1,5 +1,11 @@
 package com.lz.array;
 
+import org.assertj.core.util.Lists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 算法 -- 数组
  */
@@ -17,6 +23,36 @@ public class ArrayMid {
             result = height[left] > height[right] ?
                     Math.max(result, (right - left) * height[right--]) :
                     Math.max(result, (right - left) * height[left++]);
+        }
+        return result;
+    }
+
+    /**
+     * 15 三数之和
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3) return result;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-2; i++) {
+            if (nums[i] > 0) return result;
+            if (i >0 && nums[i] == nums[i-1]) continue;
+            int left = i +1;
+            int right = nums.length -1;
+            while (left < right) {
+                int sum = nums[i]+nums[left]+nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    while (left<right && nums[left] == nums[left+1]) left ++;
+                    while (right > left && nums[right] == nums[right -1]) right --;
+                    left ++;
+                    right --;
+                }else if (sum > 0) {
+                    right --;
+                }else {
+                    left ++;
+                }
+            }
         }
         return result;
     }
