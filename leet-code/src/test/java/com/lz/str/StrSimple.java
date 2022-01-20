17c2bef03814068629a2225c51de00e37606981d
lihao <243149100@qq.com>
2022-01-20 18:21:47 +0800

    leetcode 验证回文串 while可以和continue一起用

--- leet-code/src/test/java/com/lz/str/StrSimple.java
+++ leet-code/src/test/java/com/lz/str/StrSimple.java
@@ -221,6 +221,29 @@ public class StrSimple {
         return str.reverse().toString();
     }
 
+    /**
+     * 125. 验证回文串
+     */
+    public boolean isPalindrome(String s) {
+        if (s.equals("")) return true;
+        int i = 0;
+        int j = s.length() - 1;
+        while (i <= j) {
+            if (!Character.isLetterOrDigit(s.charAt(i))) {
+                i ++;
+                continue;
+            };
+            if (!Character.isLetterOrDigit(s.charAt(j))) {
+                j --;
+                continue;
+            };
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
 
     @Test
     public void test() {
