a9945f921ecf8ea56170020ec2289210cc7028c9
lihao <243149100@qq.com>
2022-01-31 21:03:16 +0800

    leetcode 数字变成0的操作次数

--- leet-code/src/test/java/com/lz/doublePointer/DoublePointerSimple.java
+++ leet-code/src/test/java/com/lz/doublePointer/DoublePointerSimple.java
@@ -4,4 +4,21 @@ package com.lz.doublePointer;
  * 双指针 简单
  */
 public class DoublePointerSimple {
+
+    /**
+     * 1342. 将数字变成 0 的操作次数
+     */
+    public int numberOfSteps(int num) {
+        int count = 0;
+        while (num > 0) {
+            if ((num&1)==1) {
+                num -= 1;
+                count += 1;
+            }else {
+                num = num/2;
+                count ++;
+            }
+        }
+        return count;
+    }
 }
