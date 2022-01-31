cc9236fe6ed3333fa1538d2a598911044751b720
lihao <243149100@qq.com>
2022-01-31 21:41:03 +0800

    leetcode 删除链表中的第n个节点

--- leet-code/src/test/java/com/lz/doublePointer/DoublePointerSimple.java
+++ leet-code/src/test/java/com/lz/doublePointer/DoublePointerSimple.java
@@ -1,5 +1,9 @@
 package com.lz.doublePointer;
 
+import com.lz.entity.ListNode;
+
+import java.util.HashMap;
+
 /**
  * 双指针 简单
  */
@@ -20,5 +24,36 @@ public class DoublePointerSimple {
             }
         }
         return count;
+    }
+
+    /**
+     * 19. 删除链表的倒数第 N 个结点
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode tmpNode = head;
+        int sz = 1;
+        while (tmpNode.next != null) {
+            tmpNode = tmpNode.next;
+            sz ++;
+        }
+        if (n>sz) return null;
+        int num = sz - n + 1;
+        if (num == 1) {
+            return head.next;
+        }else if (num == sz) {
+            tmpNode = head;
+            for (int i = 1; i < sz-1; i++) {
+                tmpNode = tmpNode.next;
+            }
+            tmpNode.next = null;
+            return head;
+        }else {
+            tmpNode = head;
+            for (int i = 1; i < num-1; i++) {
+                tmpNode = tmpNode.next;
+            }
+            tmpNode.next = tmpNode.next.next;
+        }
+        return head;
     }
 }
