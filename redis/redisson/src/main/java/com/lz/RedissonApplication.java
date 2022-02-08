26f4d226195bde3a016bf7c572141c13636fabae
lihao <243149100@qq.com>
2022-02-08 15:48:17 +0800

    redis 新增Redisson模块

---
+++ redis/redisson/src/main/java/com/lz/RedissonApplication.java
@@ -0,0 +1,15 @@
+package com.lz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lihao
+ */
+@SpringBootApplication
+public class RedissonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedissonApplication.class,args);
+    }
+}
