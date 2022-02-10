d3297a0e0f9110e0d2ef761aee7d765017165c5d
lihao <243149100@qq.com>
2022-02-10 13:55:14 +0800

    增加日常测试

---
+++ daily-test/daily-test-simple/src/test/java/com/lz/Test001.java
@@ -0,0 +1,18 @@
+package com.lz;
+
+import org.junit.Test;
+
+@SuppressWarnings("all")
+public class Test001 {
+
+    @Test
+    public void test001() {
+        int i = 1;
+        i = i ++;
+        int j = i ++;
+        int k = i + ++i*i++;
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+    }
+}
