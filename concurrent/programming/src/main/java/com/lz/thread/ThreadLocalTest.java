5ebb909437e4964e563e34f1fee794c81e1908b4
lihao <243149100@qq.com>
2022-02-20 22:21:26 +0800

    ThreadLocal源码实现

---
+++ concurrent/programming/src/main/java/com/lz/thread/ThreadLocalTest.java
@@ -0,0 +1,33 @@
+package com.lz.thread;
+
+/**
+ * @author lihao
+ */
+@SuppressWarnings("all")
+public class ThreadLocalTest {
+
+    //ThreadLocal变量
+    static ThreadLocal<String> localVariable = new ThreadLocal<>();
+    //print()函数
+    static void print(String str) {
+        System.out.println(str+":"+localVariable.get());
+        //localVariable.remove();
+    }
+
+    public static void main(String[] args) {
+        Thread threadOne = new Thread(() -> {
+            localVariable.set("threadOne local variable");
+            print("threadOne");
+            System.out.println("threadOne remove after"+":"+localVariable.get());
+        });
+
+        Thread threadTwo = new Thread(()->{
+            localVariable.set("threadTwo local variable");
+            print("threadTwo");
+            System.out.println("threadTwo remove after"+":"+localVariable.get());
+        });
+
+        threadOne.start();
+        threadTwo.start();
+    }
+}
