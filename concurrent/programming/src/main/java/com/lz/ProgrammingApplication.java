e3c791ef8e04ba8958893bcc75427813d14dc152
lihao <lihao-hd@blacklake.cn>
2022-03-08 17:14:54 +0800

    commit test

--- concurrent/programming/src/main/java/com/lz/ProgrammingApplication.java
+++ concurrent/programming/src/main/java/com/lz/ProgrammingApplication.java
@@ -3,6 +3,9 @@ package com.lz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lihao
+ */
 @SpringBootApplication
 public class ProgrammingApplication {
 
