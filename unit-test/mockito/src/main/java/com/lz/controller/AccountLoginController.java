189ecef2193df204362514cf2e51a0cd413a0478
lihao <243149100@qq.com>
2022-03-26 02:25:51 +0800

    Mockito 入门

---
+++ unit-test/mockito/src/main/java/com/lz/controller/AccountLoginController.java
@@ -0,0 +1,38 @@
+package com.lz.controller;
+
+
+import com.lz.dao.AccountDao;
+import com.lz.entity.Account;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+
+/**
+ * controller
+ *
+ * @author lihao on 2022/3/26
+ */
+@Slf4j
+public class AccountLoginController {
+
+    private final AccountDao accountDao;
+
+    public AccountLoginController(AccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    public String login(HttpServletRequest request) {
+        final String userName = request.getParameter("userName");
+        final String password = request.getParameter("password");
+        try {
+            Account account = accountDao.findAccount(userName, password);
+            return Objects.isNull(account) ? "/login.html" : "/index.html";
+        } catch (Exception e) {
+            log.error("报错505: ", e);
+            return "505";
+        }
+
+    }
+}
---
+++ unit-test/mockito/src/main/java/com/lz/dao/AccountDao.java
@@ -0,0 +1,13 @@
+package com.lz.dao;
+
+import com.lz.entity.Account;
+
+/**
+ * @author lihao on 2022/3/26
+ */
+public class AccountDao {
+    //不可用
+    public Account findAccount(String userName, String password) {
+        throw new UnsupportedOperationException();
+    }
+}
