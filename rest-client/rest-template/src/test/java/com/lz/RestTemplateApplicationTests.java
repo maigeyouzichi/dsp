7d4b7babf4ebd4d85d7afb8530003f8fc9734d5b
lihao <243149100@qq.com>
2022-06-21 17:31:10 +0800

    新增配置: rest-template

--- rest-client/rest-template/src/test/java/com/lz/RestTemplateApplicationTests.java
+++ rest-client/rest-template/src/test/java/com/lz/RestTemplateApplicationTests.java
@@ -1,13 +1,22 @@
 package com.lz;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+@SpringBootTest(classes = RestTemplateApplication.class)
+@RunWith(SpringRunner.class)
 class RestTemplateApplicationTests {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Test
     void contextLoads() {
+        System.out.println("地址: "+restTemplate);
     }
 
 }
