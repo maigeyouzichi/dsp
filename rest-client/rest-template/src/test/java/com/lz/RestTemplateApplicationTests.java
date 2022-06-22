fb6a0948c8b794a3112e4171f34e90c707c8d4f6
lihao <243149100@qq.com>
2022-06-22 14:59:51 +0800

    增加rest调用测试

--- rest-client/rest-template/src/test/java/com/lz/RestTemplateApplicationTests.java
+++ rest-client/rest-template/src/test/java/com/lz/RestTemplateApplicationTests.java
@@ -4,8 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.SocketTimeoutException;
+import java.util.concurrent.TimeoutException;
 
 @SpringBootTest(classes = RestTemplateApplication.class)
 @RunWith(SpringRunner.class)
@@ -16,7 +22,35 @@ class RestTemplateApplicationTests {
 
     @Test
     void contextLoads() {
-        System.out.println("地址: "+restTemplate);
+        System.out.println("地址: " + restTemplate);
+    }
+
+    @Test
+    void test001() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("x-org-id", "1");
+        httpHeaders.add("x-user-id", "2");
+        httpHeaders.add("X-User-Name", "H");
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        String url = "http://172.16.9.144:8080/v1/project";
+        HttpEntity<String> httpEntity = new HttpEntity<>("{}", httpHeaders);
+        Object res = "";
+        try {
+            res = restTemplate.postForEntity(url, httpEntity, String.class);//Read timed out
+        } catch (Exception e) {
+            System.out.println("e.message:" + e.getMessage());
+            System.out.println("是否包含要求字段: " + e.getMessage().contains("项目号不能为空"));
+            System.out.println("e.cause:" + e.getCause());
+        }
+    }
+
+    @Test
+    public void test002() {
+        String str = "[{\"message\":\"项目号重复\",\"description\":\"项目号重复\",\"sta";
+        String str2 = "项目号重复";
+        String target = "项目号重复";
+        System.out.println(str.contains(target));
+        System.out.println(str2.contains(target));
     }
 
 }
