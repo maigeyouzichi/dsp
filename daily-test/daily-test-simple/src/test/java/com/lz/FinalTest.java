9cd486f5a08a3f27972f73847d294ed9af69cb15
lihao <maigeyouzichi@gmail.com>
2022-10-08 10:16:05 +0800

    rebase remote

--- daily-test/daily-test-simple/src/test/java/com/lz/FinalTest.java
+++ daily-test/daily-test-simple/src/test/java/com/lz/FinalTest.java
@@ -3,6 +3,8 @@ package com.lz;
 import com.lz.entity.Apple;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -61,5 +63,62 @@ public class FinalTest {
         }
         if(pq.isEmpty()) return 0;
         return pq.poll();
+    }
+
+    @Test
+    public void test05() {
+        System.out.println(multiply("11", "11"));
+    }
+
+    //统计数组中位数为偶数的数字的个数
+    private int countNum(int[] arr) {
+        int count= 0;
+        for(int nu: arr) {
+            int tmp = 0;
+            while (nu > 0) {
+                nu /=10;
+                tmp++;
+            }
+            if((tmp&1) == 0) count++;
+        }
+        return count;
+    }
+
+    public String multiply(String num1, String num2) {
+        long m = convertStr2Long(num1);
+        long n = convertStr2Long(num2);
+        List<Long> list = new ArrayList<>();
+        int k = 1;
+        while(n >0) {
+            list.add(n%10 * m * k);
+            n /= 10;
+            k *= 10;
+        }
+        StringBuilder sb = new StringBuilder();
+        long lastNum = list.get(list.size() - 1);
+        long jw = 0;
+        while(lastNum > 0) {
+            long tmp = 0;
+            for(int i=0;i<list.size();i++) {
+                tmp += list.get(i)%10;
+                list.set(i, list.get(i)/10);
+            }
+            sb.append((tmp+jw)%10);
+            jw = (tmp+jw)/10;
+            lastNum /= 10;
+        }
+        return sb.reverse().toString();
+    }
+
+    private long convertStr2Long(String str) {
+        char[] chars = str.toCharArray();
+        long rns = 0;
+        long k= 1;
+        for(int i =chars.length-1;i>=0;i--) {
+            long tmp = chars[i] - '0';
+            rns += tmp * k;
+            k *= 10;
+        }
+        return rns;
     }
 }
