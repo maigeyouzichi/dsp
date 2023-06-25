102c596f44cb8825dce272c2dd845568543c2eaf
lihao <243149100@qq.com>
2023-06-25 14:52:51 +0800

    something add

--- leet-code/src/test/java/com/lz/array/ArraySimple.java
+++ leet-code/src/test/java/com/lz/array/ArraySimple.java
@@ -1,20 +1,10 @@
 package com.lz.array;
 
 import com.lz.entity.ListNode;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Set;
-import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 数组类型题目 -- 简单
